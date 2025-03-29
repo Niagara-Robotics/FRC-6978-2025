cdb7c051de79e60f5918440879d0a90caf8d6b09
Danny-Dunn <49325378+Danny-Dunn@users.noreply.github.com>
2025-03-29 06:57:53 -0400

    Adjust intake ambient threshold

--- src/main/cpp/Intake.cpp
+++ src/main/cpp/Intake.cpp
@@ -143,7 +143,7 @@ void Intake::handle_pickup_coral() {
         if(intake_sensor.get_measurement().has_value())
             if(intake_sensor.get_measurement().value().distance_mm < coral_horizontal_threshold &&
                 intake_sensor.get_measurement().value().status == grpl::LASERCAN_STATUS_VALID_MEASUREMENT &&
-                intake_sensor.get_measurement().value().ambient < 128 && 
+                intake_sensor.get_measurement().value().ambient < 650 && 
                 std::chrono::steady_clock::now() - coral_horizontal_start > std::chrono::milliseconds(600))
                 state = IntakeState::coral_vertical_b;
         break;
